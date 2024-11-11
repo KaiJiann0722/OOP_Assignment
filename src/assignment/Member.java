328c62134418714dbc45b5ec5039fab0f49157c3
KaiJiann0722 <108057611+KaiJiann0722@users.noreply.github.com>
2024-11-11 21:39:43 +0800

    Update Member.java

--- src/assignment/Member.java
+++ src/assignment/Member.java
@@ -1,14 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package assignment;
 import java.time.LocalDate;
 
-/**
- *
- * @author DingWei
- */
 public class Member {
     private static int currentMemberID = 10001;
     private String memberID;
